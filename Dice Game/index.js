408d3a833a69e19288454fced960d0804cdbe757
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2022-06-19 15:55:22 +0000

    Update index.js

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,4 +1,4 @@
-
+//First dice
 //Generating random numbers from 1-6
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
@@ -9,12 +9,14 @@ var randomImageSource1 = "images/dice" + "randomNumber + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource1);
 
 
-
-
+//Second dice
+//Generating random numbers from 1-6
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+//Selecting image from dice1.png - dice6.png
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
+//Changing image randomly
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
@@ -23,10 +25,12 @@ if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 wins";
 }
 
+//if player 2 wins
 else if(randomNumber1 < randomNumber2){
     document.querySelector("h1").innerHTML = "Player 2 wins";
 }
 
+//if player 1 = player 2
 else{
     document.querySelector("h1").innerHTML = "Draw";
 }
