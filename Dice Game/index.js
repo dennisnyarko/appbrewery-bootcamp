429faf406a8fa6032ded64d389afceeb37811acf
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2022-06-19 15:44:08 +0000

    Update index.js

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,13 +1,11 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource1 = "images/dice" + "randomNumber + ".png";
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource1);
 
-var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
