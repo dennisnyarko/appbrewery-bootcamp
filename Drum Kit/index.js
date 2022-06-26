138adcd65eadc6bb3b4782531f41f8ef52a718c6
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2022-06-26 15:38:38 +0000

    Update index.js

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -7,6 +7,6 @@ for (var i = 0; i<numberOfDrumButtons; i++){
 document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
 function handleClick() {
-    alert("I got clicked:");
-}
+     var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
 }
