a49977540def18756f9bffbab2c68babac617cef
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2022-06-28 12:17:32 +0000

    Update index.js

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -70,5 +70,9 @@ function buttonAnimation(currentKey){
   var activeButton = document.querySelector("." + currentKey);
     
   activeButton.classList.add("pressed");
+  
+    setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
 
 }
