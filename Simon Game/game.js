98c11213e15e7aac0546a1ca21e1bbd370ec4d6b
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2022-06-29 21:41:39 +0000

    Create game.js

---
+++ Simon Game/game.js
@@ -0,0 +1,12 @@
+
+buttonColors = ["red","blue","green","yellow"];
+
+gamePattern = [];
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4) ;
+    var randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor)
+}
+
