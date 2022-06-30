a7bc4427bd0fb783a1c6553ed3e5ecc43ed8f32a
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2022-06-30 14:34:56 +0000

    Update game.js

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -2,14 +2,17 @@
 var buttonColours = ["red","blue","green","yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
 $(".btn").click(function() {
+
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
+    playSound(userChosenColour);
+
 });
+
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
@@ -20,7 +23,10 @@ function nextSequence(){
 
 $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-audio.play();
+playSound(randomChosenColour);
+}
 
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
